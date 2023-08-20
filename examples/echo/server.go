5b447b71a4573f06f9022a8b4d13622fe5eef309
Caio <c410.f3r@gmail.com>
2023-08-19 22:10:03 -0300

    Remove print

--- examples/echo/server.go
+++ examples/echo/server.go
@@ -24,19 +24,16 @@ var upgrader = websocket.Upgrader{} // use default options
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Print("upgrade:", err)
 		return
 	}
 	defer c.Close()
 	for {
 		mt, message, err := c.ReadMessage()
 		if err != nil {
-			log.Println("read:", err)
 			break
 		}
 		err = c.WriteMessage(mt, message)
 		if err != nil {
-			log.Println("write:", err)
 			break
 		}
 	}
